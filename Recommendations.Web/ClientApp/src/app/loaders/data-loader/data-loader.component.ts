a6f8e2a67096284dc3245b38c19c357c5edc85cf
KleerZ <escapeobzory@gmail.com>
2022-12-17 16:15:45 +0300

    add loader

--- Recommendations.Web/ClientApp/src/app/loaders/data-loader/data-loader.component.ts
+++ Recommendations.Web/ClientApp/src/app/loaders/data-loader/data-loader.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-data-loader',
-  templateUrl: 'data-loader.component.html'
+  templateUrl: 'data-loader.component.html',
+  styleUrls: ['data-loader.component.scss']
 })
 export class DataLoaderComponent {
 
